aebb9805b9e96310f594df55c527fc93dd6dd1a1
Ahmad-shobasi <132388281+ahmad-shobasi@users.noreply.github.com>
2025-07-22 14:25:21 +0300

    Create click event to switch theme mode.
    Set button with icon mode to switch between theme in the app using theme service.

--- lifehub/src/app/layouts/topbar/topbar.ts
+++ lifehub/src/app/layouts/topbar/topbar.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject, ViewChild } from '@angular/core';
+import { Mode, ThemeService } from '../../theme/theme.service';
 
 @Component({
   selector: 'app-topbar',
   imports: [],
   templateUrl: './topbar.html',
-  styleUrl: './topbar.scss'
+  styleUrl: './topbar.scss',
 })
 export class Topbar {
-
+  themeService = inject(ThemeService);
+  @ViewChild('darkBtn') dark: any;
+  @ViewChild('lightBtn') light: any;
+  isDarkTheme: boolean = window.matchMedia('(prefers-color-scheme: dark)')
+    .matches;
+  toggleTheme() {
+    this.isDarkTheme = !this.isDarkTheme;
+    this.themeService.setThemeMode(this.isDarkTheme ? 'dark' : 'light');
+  }
 }
