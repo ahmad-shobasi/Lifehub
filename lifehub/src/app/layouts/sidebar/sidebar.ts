fdde3112bfcd7951b5e03dc00cd4affcd7483cb5
Ahmad-shobasi <132388281+ahmad-shobasi@users.noreply.github.com>
2025-07-23 11:28:44 +0300

    Set click event to side items.
    To navigate to pages components.

--- lifehub/src/app/layouts/sidebar/sidebar.ts
+++ lifehub/src/app/layouts/sidebar/sidebar.ts
@@ -1,17 +1,23 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, inject, Output } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
   imports: [],
   templateUrl: './sidebar.html',
-  styleUrl: './sidebar.scss'
+  styleUrl: './sidebar.scss',
 })
 export class Sidebar {
-  isCollapsed:boolean = false;
-  @Output() resizeSidebar = new EventEmitter()
+  isCollapsed: boolean = false;
+  private router = inject(Router);
+  @Output() resizeSidebar = new EventEmitter();
 
-  toggleSidebar(){
-    this.isCollapsed = !this.isCollapsed
+  toggleSidebar() {
+    this.isCollapsed = !this.isCollapsed;
     this.resizeSidebar.emit();
+  }
+
+  navigateTo(path: string) {
+    this.router.navigate([`/${path}`]);
   }
 }
