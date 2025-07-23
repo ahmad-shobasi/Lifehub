38a64d569796df7b0b78d23165c4cfc564cbc76e
Ahmad-shobasi <132388281+ahmad-shobasi@users.noreply.github.com>
2025-07-23 15:28:16 +0300

    Create tasks page template.
    Create template as two section:
    1: task form to create task.
    2: task list to loop throw tasks list and display it.

--- lifehub/src/app/pages/tasks/tasks.ts
+++ lifehub/src/app/pages/tasks/tasks.ts
@@ -1,11 +1,47 @@
 import { Component } from '@angular/core';
+import { Header } from '../../shared/components/header/header';
+import { TasksForm } from '../../shared/components/tasks-form/tasks-form';
+import {
+  TaskCard,
+  TaskCardModel,
+} from '../../shared/components/task-card/task-card';
+import { AbstractControl } from '@angular/forms';
+import { PriorityEnum } from '../../shared/components/priority/priority';
 
 @Component({
   selector: 'app-tasks',
-  imports: [],
+  imports: [Header, TasksForm, TaskCard],
   templateUrl: './tasks.html',
-  styleUrl: './tasks.scss'
+  styleUrl: './tasks.scss',
 })
 export class Tasks {
+  tasksList: TaskCardModel[] = [
+    {
+      title: 'new Task',
+      description: 'description of test task ',
+      priority: PriorityEnum.InProgress,
+    },
+  ];
+  createTask(form: { [key: string]: AbstractControl<any, any, any> }) {
+    const priorityValues = [
+      PriorityEnum.Todo,
+      PriorityEnum.InProgress,
+      PriorityEnum.Overdue,
+      PriorityEnum.Done,
+    ];
+    const priorityIndex = form['taskPriority'].value - 1;
 
+    const data: TaskCardModel = {
+      title: form['taskTitle'].value,
+      description: form['description'].value,
+      priority: priorityValues[priorityIndex],
+      // deleteTask: ()=> this.deleteTaskCard(1),
+    };
+    this.tasksList.push(data);
+  }
+
+  deleteTaskCard(id: number) {
+    const index = this.tasksList.findIndex((card) => card.id === id);
+    this.tasksList.slice(0, index);
+  }
 }
