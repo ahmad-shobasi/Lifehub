ef5ad80d9bb32fd9677375302d5125375e4dc4be
Ahmad-shobasi <132388281+ahmad-shobasi@users.noreply.github.com>
2025-07-26 12:21:35 +0300

    Provide calendar root in app config.

--- lifehub/src/app/app.config.ts
+++ lifehub/src/app/app.config.ts
@@ -1,12 +1,29 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  provideBrowserGlobalErrorListeners,
+  provideZonelessChangeDetection,
+} from '@angular/core';
+import { provideAnimations } from '@angular/platform-browser/animations';
+
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
-  ]
+    provideAnimations(),
+    provideRouter(routes),
+    importProvidersFrom(
+      CalendarModule.forRoot({
+        provide: DateAdapter,
+        useFactory: adapterFactory,
+      })
+    ),
+  ],
 };
