fe4cd4221da8cf28deb97e2c7400b62e35db326c
Ahmad-shobasi <132388281+ahmad-shobasi@users.noreply.github.com>
2025-07-26 12:23:59 +0300

    Create calendar view.
    Create calendar view in template page and customize it.
    Use angular-calendar stylesheet.
    Set dark mode to the calendar using cal-them  mixin from the library.

--- lifehub/src/app/pages/calendar/calendar.ts
+++ lifehub/src/app/pages/calendar/calendar.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ViewEncapsulation,
+} from '@angular/core';
+import { CalendarModule } from 'angular-calendar';
+import { CalendarView, CalendarEvent } from 'angular-calendar';
+import { addHours } from 'date-fns';
 
 @Component({
   selector: 'app-calendar',
-  imports: [],
+  imports: [CalendarModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None,
   templateUrl: './calendar.html',
-  styleUrl: './calendar.scss'
+  styleUrl: './calendar.scss',
 })
 export class Calendar {
+  view: CalendarView = CalendarView.Month;
+  CalendarView = CalendarView;
+  viewDate: Date = new Date();
+  activeDayIsOpen: boolean = true;
+  events: CalendarEvent[] = [
+    {
+      start: addHours(new Date(), 2),
+      title: 'LifeHub Kickoff',
+      color: { primary: '#007ACC', secondary: '#D1E8FF' },
+    },
+    {
+      start: new Date(),
+      title: 'Team Sync',
+      color: { primary: '#e74c3c', secondary: '#FADBD8' },
+    },
+  ];
 
+  closeOpenMonthViewDay() {
+    this.activeDayIsOpen = false;
+  }
+  setView(view: CalendarView) {
+    this.view = view;
+  }
 }
